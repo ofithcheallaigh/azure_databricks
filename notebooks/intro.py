5744a4942f2899908292cd9de3d32e29b91614c0
Seán <sofithcheallaigh@gmail.com>
2021-07-31 20:24:28 +0100

    Checking out folders

---
+++ notebooks/intro.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Notebooks Introduction
+# MAGIC * UI Intro
+# MAGIC * Magic Commands
+
+# COMMAND ----------
+
+msg = "Hello"
+
+# COMMAND ----------
+
+print(msg)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello"
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val msg = "Hello"
+
+# COMMAND ----------
+
+# File system
+%fs
+ls
+
+# COMMAND ----------
+
+# Below is the shell command
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
