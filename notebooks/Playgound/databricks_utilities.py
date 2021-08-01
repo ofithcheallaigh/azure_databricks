f9e4f75d407701dc224e03b01e8dc44d762ff1c6
Seán <sofithcheallaigh@gmail.com>
2021-08-01 22:33:37 +0100

    Updating utilities notebook

--- notebooks/Playgound/databricks_utilities.py
+++ notebooks/Playgound/databricks_utilities.py
@@ -9,3 +9,29 @@ dbutils.fs.ls('/')
 
 # COMMAND ----------
 
+for folder_name in dbutils.fs.ls('/'):
+  print(folder_name)
+# We can combine python code with the dbutilis 
+# we can do this with R and SQL also
+
+# COMMAND ----------
+
+dbutils.fs.help()
+# Help command
+
+# COMMAND ----------
+
+dbutils.fs.help("mount")
+# Getting help on the mount method
+
+# COMMAND ----------
+
+dbutils.notebook.help()
+
+# COMMAND ----------
+
+dbutils.notebook.run("./child_notebook", 20)
+# This calls out child notebook, timeout is 10 seconds
+
+# COMMAND ----------
+
