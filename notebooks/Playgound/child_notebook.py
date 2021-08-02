d3af18945787dfe2ead85eb195470e59b99cb7d3
Seán <sofithcheallaigh@gmail.com>
2021-08-02 19:28:45 +0100

    Adding use of dbutils.widgets.get to child notebook

--- notebooks/Playgound/child_notebook.py
+++ notebooks/Playgound/child_notebook.py
@@ -1,4 +1,16 @@
 # Databricks notebook source
+dbutils.widgets.text("input", "", "Send the parameter value")
+
+# COMMAND ----------
+
+input_param = dbutils.widgets.get("input")
+
+# COMMAND ----------
+
+input_param
+
+# COMMAND ----------
+
 print("I am the child notebook")
 
 # COMMAND ----------
