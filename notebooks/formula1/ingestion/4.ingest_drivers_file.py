e7203fb9934e739ce78e8487c3e3a71801ceefe1
Seán <sofithcheallaigh@gmail.com>
2021-08-20 21:37:06 +0100

    Have renamed columns and added new columns.

--- notebooks/formula1/ingestion/4.ingest_drivers_file.py
+++ notebooks/formula1/ingestion/4.ingest_drivers_file.py
@@ -11,6 +11,40 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType, 
 
 # COMMAND ----------
 
+# MAGIC %md we will need to define two schema -- an inner one for the name, and an outer one for the whole thing
+
+# COMMAND ----------
+
+name_schema = StructType(fields=[StructField("forename", StringType(), True),
+                                 StructField("surname", StringType(), True)
+])
+
+# COMMAND ----------
+
+drivers_schema = StructType(fields=[StructField("driverId", IntegerType(), False),
+                                    StructField("driverRef", StringType(), True),
+                                    StructField("number", IntegerType(), True),
+                                    StructField("code", StringType(), True),
+                                    StructField("name", name_schema),
+                                    StructField("dob", DateType(), True),
+                                    StructField("nationality", StringType(), True),
+                                    StructField("url", StringType(), True)
+  
+  
+])
+
+# COMMAND ----------
+
+drivers_df = spark.read\
+.schema(drivers_schema)\
+.json("/mnt/formula1dlsof/raw/drivers.json")
+
+# COMMAND ----------
+
+drivers_df.printSchema()
+
+# COMMAND ----------
+
 # MAGIC %md #### Step 2: Rename columns and add new columns
 # MAGIC 1. driverId renamed to driver_id
 # MAGIC 2. driverRef renamed to driver_ref
@@ -19,7 +53,18 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType, 
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col, concat, current_timestamp, lit
 
+# COMMAND ----------
+
+drivers_with_columns_df = drivers_df.withColumnRenamed("driverId", "driver_id") \
+                                    .withColumnRenamed("driverRef", "driver_ref") \
+                                    .withColumn("ingestion_date", current_timestamp()) \
+                                    .withColumn("name", concat(col("name.forename"), lit(" "), col("name.surname")))
+
+# COMMAND ----------
+
+display(drivers_with_columns_df)
 
 # COMMAND ----------
 
