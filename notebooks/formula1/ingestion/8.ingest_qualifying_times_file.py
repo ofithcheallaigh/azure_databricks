3c7bd88ae374a95e81b34ef428d5f17d53684a37
Seán <sofithcheallaigh@gmail.com>
2021-08-22 17:22:08 +0100

    Read in, column names changed and added ingestion date, and written to parquet

--- notebooks/formula1/ingestion/8.ingest_qualifying_times_file.py
+++ notebooks/formula1/ingestion/8.ingest_qualifying_times_file.py
@@ -3,52 +3,36 @@
 
 # COMMAND ----------
 
-# MAGIC %md #### Step 1: Read the CSV file using the spark dataframe reader API
-
-# COMMAND ----------
-
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 from pyspark.sql.functions import current_timestamp, col
 
 # COMMAND ----------
 
-lap_times_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
-                                     StructField("driverId", IntegerType(), True),
-                                     StructField("lap", IntegerType(), True),
-                                     StructField("position", IntegerType(), True),
-                                     StructField("time", StringType(), True),
-                                     StructField("milliseconds", IntegerType(), True) 
-])
+qualifying_schema = StructType(fields=[StructField("qualifyId", IntegerType(), False),
+                                      StructField("raceId", IntegerType(), True),
+                                      StructField("driverId", IntegerType(), True),
+                                      StructField("constructorId", IntegerType(), True),
+                                      StructField("number", IntegerType(), True),
+                                      StructField("position", IntegerType(), True),
+                                      StructField("q1", StringType(), True),
+                                      StructField("q2", StringType(), True),  
+                                      StructField("q3", StringType(), True)])
 
 # COMMAND ----------
 
-lap_times_df = spark.read\
-.schema(lap_times_schema)\
-.csv("/mnt/formula1dlsof/raw/lap_times")
+qualifying_df = spark.read.schema(qualifying_schema).option("multiLine", True).json("/mnt/formula1dlsof/raw/qualifying")
 
 # COMMAND ----------
 
-# MAGIC %md #### Step 2: Rename columns and add new column
-# MAGIC 1. rename driverId and raceId
-# MAGIC 2. Add `ingestion_date` with current timestamp
+final_df = qualifying_df.withColumnRenamed("raceId", "race_id")\
+                        .withColumnRenamed("driverId", "driver_id")\
+                        .withColumnRenamed("qualifyId", "qualify_id")\
+                        .withColumnRenamed("constructorId", "constructor_id")\
+                        .withColumn("ingestion_date", current_timestamp())
 
 # COMMAND ----------
 
-final_df = lap_times_df.withColumnRenamed("raceId", "race_id")\
-                       .withColumnRenamed("driverId", "driver_id")\
-                       .withColumn("ingestion_date", current_timestamp())
-
-# COMMAND ----------
-
-# MAGIC %md #### Write the output to the process container in pargqet format
-
-# COMMAND ----------
-
-final_df.write.mode("overwrite").parquet("/mnt/formula1dlsof/process/lap_times")
-
-# COMMAND ----------
-
-display(spark.read.parquet("/mnt/formula1dlsof/process/lap_times"))
+final_df.write.mode("overwrite").parquet("/mnt/formula1dlsof/process/qualifying")
 
 # COMMAND ----------
 
